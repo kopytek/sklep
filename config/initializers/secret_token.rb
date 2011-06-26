4ce4eedd928149dd0b11cbd92b3b44d5e611804e
kopytek <tylkonieto1@gmail.com>
2011-06-26 12:06:36 +0200

    Clean rails structure using html5 boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sklep::Application.config.secret_token = '6aa5fc30f2928f6cf1610f12894fdd29e35828aea444d54a6208f319c7051b67a0f3a6f45d874694a9468d3a5b28b25e5b2fe9cbbae07ff337923c074fd40d6b'
